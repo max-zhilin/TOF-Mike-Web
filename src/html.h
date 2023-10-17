405d3ede99456b00b04567c781c87c817b6233dd
Max Zhilin <max_zhilin@mail.ru>
2023-10-17 11:14:03 +0300

    first commit

---
+++ src/html.h
@@ -0,0 +1,40 @@
+/*
+  ESP32 HTML WebServer Page Code
+  http:://www.electronicwings.com
+*/
+
+const char html_page[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+  <style>
+    body {font-family: sans-serif;}
+    h1 {text-align: center; font-size: 30px;}
+    p {text-align: center; color: #120a8f; font-size: 60px;}
+  </style>
+
+<body>
+  <h1>VL53L1X Laser Distance Measurement. BuzNel Lab (c)</h1><br>
+  <p>Pallet Height : <span id="_palHeight">0</span> cm</p>
+  <p>Mounted Sensor Height : <span id="_sensHeight">0</span> cm</p>
+  <p>Real Measured Distance : <span id="_realDist">0</span> cm</p>
+ 
+
+<script>
+  setInterval(function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const text = this.responseText;
+        const myArr = JSON.parse(text);
+        document.getElementById("_palHeight").innerHTML = myArr[0];
+        document.getElementById("_sensHeight").innerHTML = myArr[1];
+	document.getElementById("_realDist").innerHTML = myArr[2];
+      }
+    };
+    xhttp.open("GET", "readHightData", true);
+    xhttp.send();
+  },50);
+</script>
+</body>
+</html>
+)rawliteral";
---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+
+// https://www.electronicwings.com/esp32/vl53l0x-sensor-interfacing-with-esp32
+// Nln works! 
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "html.h"
+#include <Wire.h>
+#include <VL53L1X.h>   //Pololu
+
+ESP8266WebServer server(80);
+VL53L1X sensor;
+
+const char* ssid     = "AndroidAP8459";
+const char* password = "tyyu2849";
+//const char* ssid     = "Uni_Mkl";
+//const char* password = "AAA123abcd";
+//const char* ssid     = "nelen5g";
+//const char* password = "rKYHcc3a";
+//const char* ssid     = "Mkl Nt8";
+//const char* password = "AAA123ABCD";
+
+int measuredDist;
+int sensorHeight = 300;               //установочная высота сенсора от пола в cm
+int palletHeight;                     //вычисленная высота паллета 
+
+
+void MainPage() {
+  //String _html_page = html_page;              /*Read The HTML Page*/
+  server.send(200, "text/html", html_page);  /*Send the code to the web server*/
+}
+
+void MeasuredData() {
+  Serial.print("MeasuredData()");
+  String data = "[\"" + String(palletHeight) + "\", \"" + String(sensorHeight) + "\", \"" + String(measuredDist) + "\"]";
+  Serial.print(data);
+  server.send(200, "text/plane", data);
+  Serial.print("data sent");
+}
+
+void setup(void){
+  Serial.begin(9600);                 /*Set the baudrate to 115200*/
+  delay(3000);
+
+  //-------------VL53L1X Pololu Lib---------------
+  Wire.begin();
+  Wire.setClock(400000); // use 400 kHz I2C
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1);
+  }
+  sensor.setDistanceMode(VL53L1X::Long);
+  sensor.setMeasurementTimingBudget(50000); 
+  sensor.startContinuous(100);
+  
+  //--------------WiFi Connection---------------
+  WiFi.mode(WIFI_STA);                  /*Set the WiFi in STA Mode*/
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  delay(1000);                          /*Wait for 1000mS*/
+  while(WiFi.waitForConnectResult() != WL_CONNECTED){Serial.print(".");} /*Wait while connecting to WiFi*/
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("Your Local IP address is: ");
+  Serial.println(WiFi.localIP());            /*Print the Local IP*/
+  
+  //---------------WEB page---------------------
+  server.on("/", MainPage);                  /*Display the Web/HTML Page*/
+  server.on("/readHightData", MeasuredData); /*Display the hight value(cm)*/
+  server.begin();                            /*Start Server*/
+  delay(1000);                               /*Wait for 1000mS*/
+}
+
+void loop(void){
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  measuredDist =  sensor.read() / 10;
+  palletHeight = sensorHeight - measuredDist;
+  Serial.println("Pallet h(сm): " + String(palletHeight) + "  Sensor h(сm): " + String(sensorHeight) + "  Distance (сm): " + String(measuredDist));  
+  server.handleClient();
+  //Serial.println();
+  delay(1000);
+}
